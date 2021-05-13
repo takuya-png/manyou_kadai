d2ee207f04c86b1e85ed541af192930fb5cd304f
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-05-13 10:26:32 +0900

    System Specをタスク機能に対して書く

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,6 +3,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
+        task = FactoryBot.create(:task, title: 'task')
+        visit tasks_path
+        expect(page).to have_content 'task'
       end
     end
   end

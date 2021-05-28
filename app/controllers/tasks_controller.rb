fc4d14cc6aa70a32ea47ee3d4a0152515a1e26d8
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-05-28 12:56:22 +0900

    current_userのみを表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if params[:back]
         render :new
     else

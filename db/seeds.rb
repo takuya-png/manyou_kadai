d33e76c683742afebc6b021fd717cdfd0841b08c
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-07 01:07:33 +0900

    herokuデプロイ

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,45 +11,45 @@ Label.create([
   { name: '楽'},
   ])
 
-i = 1
-10.times do
-  name = ('a'..'z').to_a.shuffle[1..5].join
-  User.create(
-    name: name,
-    email: "#{i}@example.com",
-    password: "password",
-    admin: false
-  )
-  i += 1
-end
+# i = 1
+# 10.times do
+#   name = ('a'..'z').to_a.shuffle[1..5].join
+#   User.create(
+#     name: name,
+#     email: "#{i}@example.com",
+#     password: "password",
+#     admin: false
+#   )
+#   i += 1
+# end
 
-10.times do
-  i = 1
-  d1 = Date.parse("2021/04/13")
-  d2 = Date.parse("2021/08/30")
-  date = Random.rand(d1..d2)
-  title = ('a'..'z').to_a.shuffle[1..11].join
-  content = ('A'..'Z').to_a.shuffle[1..15].join
-  status = Random.rand(1..2)
-  priority = Random.rand(1..2)
-  user_id = Random.rand(1..8)
-  Task.create(
-    title: title,
-    content: content,
-    endtime_at:  date,
-    status: status,
-    priority: priority,
-    user_id: user_id
-  )
-  i += 1
-end
+# 10.times do
+#   i = 1
+#   d1 = Date.parse("2021/04/13")
+#   d2 = Date.parse("2021/08/30")
+#   date = Random.rand(d1..d2)
+#   title = ('a'..'z').to_a.shuffle[1..11].join
+#   content = ('A'..'Z').to_a.shuffle[1..15].join
+#   status = Random.rand(1..2)
+#   priority = Random.rand(1..2)
+#   user_id = Random.rand(1..8)
+#   Task.create(
+#     title: title,
+#     content: content,
+#     endtime_at: date,
+#     status: status,
+#     priority: priority,
+#     user_id: user_id
+#   )
+#   i += 1
+# end
 
-task_id = 1
-10.times do
-  label_id = Random.rand(1..9)
-  TaskLabel.create(
-    task_id: task_id,
-    label_id: label_id
-  )
-  task_id += 1
-end 
+# task_id = 1
+# 10.times do
+#   label_id = Random.rand(1..9)
+#   TaskLabel.create(
+#     task_id: task_id,
+#     label_id: label_id
+#   )
+#   task_id += 1
+# end 

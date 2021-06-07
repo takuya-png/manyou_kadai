dfd1def6e804bc3dbd46ba605463c238a8ac6168
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-07 21:37:41 +0900

    ラベルの絞り込み機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,20 +2,32 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    # binding.irb
-    @tasks = Task.order(id: :desc)
-    @tasks = Task.order(expired_at: :desc) if params[:sort_expired]
-    @tasks = Task.order(priority: :desc) if params[:sort_priority]
-
-    if params[:task]
-      @tasks = @tasks.search_title(params[:task][:search_title]) if params[:task][:search_title].present?
-      @tasks = @tasks.search_status(params[:task][:search_status]) if params[:task][:search_status] != ""
-      @tasks = @tasks.search_priority(params[:search_priority]) if params[:task][:search_priority].present?
+    if params[:sort_expired]
+      @tasks = current_user.tasks.order(expired_at: :desc).page(params[:page]).per(5)
+    elsif params[:sort_priority]
+      @tasks = current_user.tasks.order(priority: :desc).page(params[:page]).per(5)
+    elsif params[:search]
+      if params[:search_title].present? && params[:search_status].present? && params[:search_label].present?
+        @tasks = current_user.tasks.search_title(params[:search_title]).search_status(params[:search_status]).search_label(params[:search_label]).page(params[:page]).per(5)
+      elsif params[:search_title].present? && params[:search_status].present?
+        @tasks = current_user.tasks.search_title(params[:search_title]).search_status(params[:search_status]).page(params[:page]).per(5)
+      elsif params[:search_status].present? && params[:search_label].present?
+        @tasks = current_user.tasks.search_status(params[:search_status]).search_label(params[:search_label]).page(params[:page]).per(5)
+      elsif params[:search_title].present? && params[:search_label].present?
+        @tasks = current_user.tasks.search_title(params[:search_title]).search_label(params[:search_label]).page(params[:page]).per(5)
+      elsif params[:search_title].present?
+        @tasks = current_user.tasks.search_title(params[:search_title]).page(params[:page]).per(5)
+      elsif params[:search_status].present?
+        @tasks = current_user.tasks.search_status(params[:search_status]).page(params[:page]).per(5)
+      elsif params[:search_label].present?
+        @tasks = current_user.tasks.search_label(params[:search_label]).page(params[:page]).per(5)
+      else
+        @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(5)
+      end
+    else
+      @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(5)
     end
-
-    @tasks = Kaminari.paginate_array(current_user.tasks.order(id: :desc)).page(params[:page]).per(3)
   end
-
   def new
     @task = Task.new
   end

d846c2bd78935ab8fea54acc5a82be121be6f76f
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-07 22:34:19 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
-  end
-  resources :users, only: [:index, :new, :create, :edit, :update, :show]
   resources :sessions, only: [:new, :create, :destroy]
-  root 'tasks#index'
-  resources :tasks do
-    collection do
-      post :confirm
-    end
+  resources :users, only: [:new, :create, :show]
+  namespace :admin do
+  resources :users
   end
+  root 'tasks#index'
+  resources :tasks
 end
